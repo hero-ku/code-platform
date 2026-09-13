e6079e1780046e52e9bc292fde692e192a4661fe
Andrew Lynch <lfs68@txstate.edu>
2026-09-13 00:37:54 -0500

    Add simple RabbitMQ consumption test

--- worker/src/main.go
+++ worker/src/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	channel, err := conn.Channel()
+	failOnError(err, "Failed to get channel")
+	defer channel.Close()
+
+	queue, err := channel.QueueDeclare("Programs", true, false, false, false,
+		amqp.Table{
+			amqp.QueueTypeArg: amqp.QueueTypeClassic,
+		},
+	)
+	failOnError(err, "Failed to create queue")
+
+	msgs, err := channel.Consume(queue.Name, "", true, false, false, false, nil)
+
+	for delivery := range msgs {
+		log.Printf("%s", delivery.Body)
+	}
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s:%s", msg, err)
+	}
 }
